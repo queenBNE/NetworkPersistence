bbf5c1f2a9a26d4059414c5bff4a0f907d8bbb5b
queenBNE <jacobienc@gmail.com>
2015-03-19 18:03:50 +1100

    Experiments and data corresponding to Table 2 of 'Persistent Homology of Collaboration Networks'.

--- NetworkPersistence/experiments/network_persistence_clique_cpx/AstroPhysics.java
+++ NetworkPersistence/experiments/network_persistence_clique_cpx/AstroPhysics.java
@@ -15,25 +15,37 @@ import au.edu.rmit.javaplex.graph.io.GraphReader;
 import au.edu.rmit.javaplex.homology.filtration.DecreasingMapConverter;
 import au.edu.rmit.javaplex.plex4.streams.impl.CliqueComplexWeightedStream;
 
-public class HighEnergyPhysics {
-	final static String filename = "/Users/jacobien/Git/PersistentHomology/NetworkPersistence/Networks/highEnergyPhysics.csv";
+public class AstroPhysics {
+	final static String filename = "/Users/jacobien/Git/PersistentHomology/NetworkPersistence/Networks/astroPhysics.csv";
 	final static String sep = ",";
 	final static Boolean hasHeader = false;
 	final static Boolean directed = false;
 	
-	final static double maxWeight = 5;
+	double maxWeight = 0;
 	final static int maxDim = 3;
 	
 	@Test
 	public void computePersistence() throws IOException{
 		System.out.println("Reading graph");
 		UndirectedWeightedListGraph graph = GraphReader.getUndirectedWeightedGraph(filename, sep, hasHeader);
+		int n = graph.getNumVertices();
+		int m = graph.getNumEdges();
+		System.out.println(String.format("Graph with %s nodes and %s edges", n,m));
+		
 		Set<Double> weights = graph.getWeights();
+		for(Double weight : weights)
+			maxWeight = Math.max(maxWeight, weight);
+		maxWeight = maxWeight + 1;
 		weights.add(maxWeight);
+		System.out.println("Creating filtration converter");
 		FiltrationConverter filtrationConverter = new DecreasingMapConverter(weights);
+		System.out.println("Creating clique complex stream");
 		CliqueComplexWeightedStream stream = new CliqueComplexWeightedStream(graph, maxDim, filtrationConverter);
+		System.out.println("Finalizing clique complex stream");
 		stream.finalizeStream();
+		System.out.println("Obtaining default simplicial algorithm");
 		final AbstractPersistenceAlgorithm<Simplex> algorithm = Plex4.getDefaultSimplicialAlgorithm(maxDim);
+		System.out.println("Computing intervals");
 		BarcodeCollection<Double> intervals = algorithm.computeIntervals(stream);
 		System.out.println("JavaPlex: Decreasing");
 		System.out.println(intervals);
--- NetworkPersistence/experiments/network_persistence_clique_cpx/CondensedMatter.java
+++ NetworkPersistence/experiments/network_persistence_clique_cpx/CondensedMatter.java
@@ -15,28 +15,35 @@ import au.edu.rmit.javaplex.graph.io.GraphReader;
 import au.edu.rmit.javaplex.homology.filtration.DecreasingMapConverter;
 import au.edu.rmit.javaplex.plex4.streams.impl.CliqueComplexWeightedStream;
 
-public class HighEnergyPhysics {
-	final static String filename = "/Users/jacobien/Git/PersistentHomology/NetworkPersistence/Networks/highEnergyPhysics.csv";
+public class CondensedMatter {
+	final static String filename = "/Users/jacobien/Git/PersistentHomology/NetworkPersistence/Networks/condensedMatter.csv";
 	final static String sep = ",";
 	final static Boolean hasHeader = false;
 	final static Boolean directed = false;
 	
-	final static double maxWeight = 5;
-	final static int maxDim = 3;
+	double maxWeight = 0;
+	final static int maxDim = 2;
 	
 	@Test
 	public void computePersistence() throws IOException{
 		System.out.println("Reading graph");
 		UndirectedWeightedListGraph graph = GraphReader.getUndirectedWeightedGraph(filename, sep, hasHeader);
+		int n = graph.getNumVertices();
+		int m = graph.getNumEdges();
+		System.out.println(String.format("Graph with %s nodes and %s edges", n,m));
+		
 		Set<Double> weights = graph.getWeights();
-		weights.add(maxWeight);
+		for(Double weight : weights)
+			maxWeight = Math.max(maxWeight, weight);
+		maxWeight = maxWeight + 1;
+		weights.add(maxWeight);		
 		FiltrationConverter filtrationConverter = new DecreasingMapConverter(weights);
 		CliqueComplexWeightedStream stream = new CliqueComplexWeightedStream(graph, maxDim, filtrationConverter);
 		stream.finalizeStream();
 		final AbstractPersistenceAlgorithm<Simplex> algorithm = Plex4.getDefaultSimplicialAlgorithm(maxDim);
 		BarcodeCollection<Double> intervals = algorithm.computeIntervals(stream);
 		System.out.println("JavaPlex: Decreasing");
-		System.out.println(intervals);
+		//System.out.println(intervals);
 		System.out.println(intervals.getBettiNumbers());
 	}
 }
